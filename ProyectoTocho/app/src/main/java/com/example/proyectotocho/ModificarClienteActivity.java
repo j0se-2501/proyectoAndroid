a6a004c3dc84cc0971fbb86323dd55e4f5536b9f
j0se-2501 <jolufer23@gmail.com>
2023-11-21 20:14:52 +0100

    metodos del CRUD de usuarios

--- ProyectoTocho/app/src/main/java/com/example/proyectotocho/ModificarClienteActivity.java
+++ ProyectoTocho/app/src/main/java/com/example/proyectotocho/ModificarClienteActivity.java
@@ -2,13 +2,85 @@ package com.example.proyectotocho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ModificarClienteActivity extends AppCompatActivity {
+
+    private EditText editTextClienteId;
+    private EditText editTextNuevoCorreo;
+    private EditText editTextNuevaContrasenna;
+    private Button btnModificarCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modificar_cliente);
+
+        editTextClienteId = findViewById(R.id.editTextID);
+        editTextNuevoCorreo = findViewById(R.id.editTextCorreo);
+        editTextNuevaContrasenna = findViewById(R.id.editTextContrasena);
+        btnModificarCliente = findViewById(R.id.btnModificarCliente);
+
+        btnModificarCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modificarCliente();
+            }
+        });
+    }
+
+    private void modificarCliente() {
+        // Obtén los datos para la modificación del producto
+        String clienteIdStr = editTextClienteId.getText().toString();
+        String nuevoCorreo = editTextNuevoCorreo.getText().toString();
+        String nuevaContrasenna = editTextNuevaContrasenna.getText().toString();
+
+        // Verifica si se ingresó el ID del producto
+        if (clienteIdStr.isEmpty()) {
+            Toast.makeText(this, "Ingrese el ID del usuario a modificar", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int clienteId = Integer.parseInt(clienteIdStr);
+
+        // Accede a la base de datos
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // Verifica si el producto con el ID dado existe
+        String[] projection = {"id"};
+        String selection = "id = ?";
+        String[] selectionArgs = {String.valueOf(clienteId)};
+        Cursor cursor = db.query("usuarios", projection, selection, selectionArgs, null, null, null);
+
+        if (cursor.getCount() > 0) {
+            // El producto existe, procede con la modificación
+            ContentValues values = new ContentValues();
+            if (!nuevoCorreo.isEmpty()) values.put("correo", nuevoCorreo);
+            if (!nuevaContrasenna.isEmpty()) values.put("contraseña", nuevaContrasenna);
+
+            // Realiza la actualización del producto
+            int numRowsUpdated = db.update("usuarios", values, "id=?", new String[]{String.valueOf(clienteId)});
+
+            if (numRowsUpdated > 0) {
+                Toast.makeText(this, "Usuario modificado correctamente", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No se pudo modificar el usuario. Verifique el ID.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // El cliente no existe
+            Toast.makeText(this, "El usuario con el ID proporcionado no existe", Toast.LENGTH_SHORT).show();
+        }
+
+        // Cierra el cursor y la base de datos
+        cursor.close();
+        db.close();
     }
 }
--- ProyectoTocho/app/src/main/java/com/example/proyectotocho/MostarrClienteActivity.java
+++ ProyectoTocho/app/src/main/java/com/example/proyectotocho/MostarrClienteActivity.java
@@ -2,13 +2,63 @@ package com.example.proyectotocho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MostarrClienteActivity extends AppCompatActivity {
+
+    private ListView listViewClientes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostarr_cliente);
+
+        listViewClientes = findViewById(R.id.listViewClientes);
+
+        mostrarClientes();
+    }
+
+    private void mostrarClientes() {
+        // Accede a la base de datos
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        // Realiza una consulta para obtener todos los productos
+        Cursor cursor = db.query("usuarios", null, null, null, null, null, null);
+
+// Verifica si hay productos en la base de datos
+        if (cursor.getCount() > 0) {
+            // Procesa los resultados de la consulta
+            int idIndex = cursor.getColumnIndex("id");
+            int correoIndex = cursor.getColumnIndex("correo");
+
+            ArrayList<String> clientesList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                // Verifica si las columnas existen en el conjunto de resultados
+                if (idIndex >= 0 && correoIndex >= 0) {
+                    int id = cursor.getInt(idIndex);
+                    String correo = cursor.getString(correoIndex);
+
+                    // Agrega la información del producto a la lista
+                   clientesList.add("ID: " + id + ", Correo: " + correo);
+                } else {
+                    // Muestra un mensaje de error si alguna columna no existe
+                    Toast.makeText(this, "Error al obtener datos de la base de datos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            // Muestra la lista de clientes en el ListView
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, clientesList);
+            listViewClientes.setAdapter(adapter);
+        } else {
+            Toast.makeText(this, "No hay clientes en la base de datos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
