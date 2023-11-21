67f05fb7b4f9f45e186b814e94d4db3763edf38c
j0se-2501 <jolufer23@gmail.com>
2023-11-21 16:00:58 +0100

    Auto stash before merge of "main" and "origin/main"

--- ProyectoTocho/app/src/main/java/com/example/proyectotocho/AdminClientesActivity.java
+++ ProyectoTocho/app/src/main/java/com/example/proyectotocho/AdminClientesActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AdminClientesActivity extends AppCompatActivity {
 
