0e2ce19627bca87543f98205c8dfb5297f561860
j0se-2501 <jolufer23@gmail.com>
2024-02-16 16:23:03 +0100

    feature: se recuperan los productos de un pedido

--- ProyectoTocho/app/src/main/java/com/example/proyectotocho/PedidosAdapter.java
+++ ProyectoTocho/app/src/main/java/com/example/proyectotocho/PedidosAdapter.java
@@ -1,7 +1,9 @@
 package com.example.proyectotocho;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
@@ -49,14 +51,59 @@ public class PedidosAdapter extends RecyclerView.Adapter<PedidosAdapter.ViewHold
 
         Pedido pedido = pedidos.get(position);
 
-
-
-
         holder.textViewId.setText(String.valueOf(pedido.getId()));
         holder.textViewFecha.setText(pedido.getFecha());
         holder.textViewPrecio.setText(String.valueOf(pedido.getPrecio()));
         holder.textViewDireccion.setText(pedido.getDireccion());
 
+        holder.botonVerProductos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DbHelper dbHelper = new DbHelper(context);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.rawQuery("SELECT p.nombre, pap.cantidad_producto, p.precio * pap.cantidad_producto AS precio_total " +
+                        "FROM pedidos_a_piezas pap " +
+                        "INNER JOIN piezas p ON pap.id_producto = p.id " +
+                        "WHERE pap.id_pedido = ?;", new String[]{String.valueOf(pedido.getId())});
+                // Crear un StringBuilder para construir el texto del dialog
+                StringBuilder dialogText = new StringBuilder();
+
+                // Iterar sobre el cursor y construir el texto del dialog
+                if (cursor != null && cursor.moveToFirst()) {
+                    double precioSumado=0;
+                    do {
+
+                        @SuppressLint("Range") String nombrePieza = cursor.getString(cursor.getColumnIndex("nombre"));
+                        @SuppressLint("Range") int cantidad = cursor.getInt(cursor.getColumnIndex("cantidad_producto"));
+                        @SuppressLint("Range") double precioTotal = cursor.getDouble(cursor.getColumnIndex("precio_total"));
+                        precioSumado+=precioTotal;
+                        dialogText.append(nombrePieza).append("\n - Cantidad: ").append(cantidad).append("\n - Precio total: ").append(precioTotal).append("€\n\n");
+                    } while (cursor.moveToNext());
+                    dialogText.append("Total: ").append(precioSumado).append("€");
+
+                    cursor.close();
+                }
+
+                // Crear el AlertDialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Productos del pedido");
+                builder.setMessage(dialogText.toString());
+
+                // Añadir botón de aceptar
+                builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Cerrar el dialog
+                        dialog.dismiss();
+                    }
+                });
+
+                // Mostrar el AlertDialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
 
 
     }
@@ -74,6 +121,8 @@ public class PedidosAdapter extends RecyclerView.Adapter<PedidosAdapter.ViewHold
         TextView textViewPrecio;
         TextView textViewDireccion;
 
+        ImageButton botonVerProductos;
+
         ViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -81,6 +130,7 @@ public class PedidosAdapter extends RecyclerView.Adapter<PedidosAdapter.ViewHold
             textViewFecha = itemView.findViewById(R.id.textViewFechaContenido);
             textViewPrecio = itemView.findViewById(R.id.textViewPrecioContenido);
             textViewDireccion = itemView.findViewById(R.id.textViewDireccionContenido);
+            botonVerProductos = itemView.findViewById(R.id.btnVerProductos);
         }
     }
 
