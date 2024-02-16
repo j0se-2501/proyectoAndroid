420b022ac832bba8bfc41b02bd201aa92f3c5398
j0se-2501 <jolufer23@gmail.com>
2024-02-16 17:35:00 +0100

    bugfix: invitado no peta

--- ProyectoTocho/app/src/main/java/com/example/proyectotocho/InvitadoActivity.java
+++ ProyectoTocho/app/src/main/java/com/example/proyectotocho/InvitadoActivity.java
@@ -1,9 +1,12 @@
 package com.example.proyectotocho;
 
+import static com.example.proyectotocho.MainActivity.userId;
+
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -12,8 +15,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class InvitadoActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,121 +28,137 @@ public class InvitadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
         Intent intent = new Intent(this, VistaPorCategoriaInvitado.class);
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.color_notif_bar));
-        // Obtener el correo del usuario que ha iniciado sesión
-        String userEmail = getIntent().getStringExtra("USER_EMAIL");
-        TextView userEmailTextView = findViewById(R.id.userEmailTextView);
+
+        String userId = getIntent().getStringExtra("USER_ID");
 
 
+        /*DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
 
+        try {
+            Cursor cursor = db.rawQuery("SELECT nombre FROM usuarios WHERE id = ?", new String[]{userId});
 
-        // Dividir el correo electrónico en dos partes: nombreDeUsuario y dominio
-        String[] parts = userEmail.split("@");
-
-        // Obtener solo la parte antes de '@'
-        String nombreDeUsuario = parts[0];
-
-        // Mostrar el correo en un TextView
-
-        userEmailTextView.setText("Bienvenido, invitado.");
-
-        // Configurar OnClickListener para cada botón
-        ImageButton motorButton = findViewById(R.id.imageButton);
-        motorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                intent.putExtra("CATEGORIA", "motor");
-                startActivity(intent);
+            if (cursor.moveToFirst()) {
+                String nombreuser = cursor.getString(0);
+                userEmailTextView.setText("Bienvenido, " + nombreuser + ".");
+            } else {
+                Log.e("uwu", "Cursor vacío, no se encontró el usuario con ID: " + userId);
             }
+
+            cursor.close();
+        } catch (Exception e) {
+            Log.e("owo", "Error en la consulta de la base de datos", e);
+        } finally {
+            db.close();
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+
+        ImageButton motorButton = findViewById(R.id.imageButton);
+
+        motorButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "motor");
+            startActivity(intent);
         });
 
         ImageButton transmisionButton = findViewById(R.id.imageButton2);
-        transmisionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "transmision");
-                startActivity(intent);
-            }
+        transmisionButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "transmision");
+            startActivity(intent);
         });
 
         ImageButton sobrealimentacionButton = findViewById(R.id.imageButton3);
-        sobrealimentacionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "sobrealimentacion");
-                startActivity(intent);
-            }
+        sobrealimentacionButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "sobrealimentacion");
+            startActivity(intent);
         });
 
         ImageButton neumaticosButton = findViewById(R.id.imageButton4);
-        neumaticosButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "neumaticos");
-                startActivity(intent);
-            }
+        neumaticosButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "neumaticos");
+            startActivity(intent);
         });
 
         ImageButton llantasButton = findViewById(R.id.imageButton5);
-        llantasButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "llantas");
-                startActivity(intent);
-            }
+        llantasButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "llantas");
+            startActivity(intent);
         });
 
         ImageButton suspensionButton = findViewById(R.id.imageButton6);
-        suspensionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "suspension");
-                startActivity(intent);
-            }
+        suspensionButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "suspension");
+            startActivity(intent);
         });
 
         ImageButton frenosButton = findViewById(R.id.imageButton7);
-        frenosButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "frenos");
-                startActivity(intent);
-            }
+        frenosButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "frenos");
+            startActivity(intent);
         });
 
         ImageButton carroceriaButton = findViewById(R.id.imageButton8);
-        carroceriaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-
-                intent.putExtra("CATEGORIA", "carroceria");
-                startActivity(intent);
-            }
+        carroceriaButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "carroceria");
+            startActivity(intent);
         });
 
         ImageButton electronicaButton = findViewById(R.id.imageButton9);
-        electronicaButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                intent.putExtra("CATEGORIA", "electronica");
-                startActivity(intent);
-            }
+        electronicaButton.setOnClickListener(view -> {
+            intent.putExtra("CATEGORIA", "electronica");
+            startActivity(intent);
         });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        ImageButton drawerButton = findViewById(R.id.buttonAbrirDrawer);
+
+        drawerButton.setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
+    }
+
+
+    public void onProfileClick(MenuItem menuItem) {
+
+    }
+
+    public void onFavoritesClick(MenuItem menuItem) {
+
+    }
+
+    public void onTiendaClick(MenuItem menuItem) {
+
+    }
+    public void onCarritoClick(MenuItem menuItem) {
+
+    }
+    public void onCerrarSesionClick(MenuItem menuItem) {
+        Intent intentCerrarSesion = new Intent(InvitadoActivity.this, MainActivity.class);
+        startActivity(intentCerrarSesion);
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
